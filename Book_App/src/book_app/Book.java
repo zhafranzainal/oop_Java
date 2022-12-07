7a22687255bec01b17d0fde3132339ebc0b7612d
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-12-07 19:05:37 +0800

    [HOT: 2022(1)]

--- Book_App/src/book_app/Book.java
+++ Book_App/src/book_app/Book.java
@@ -3,9 +3,9 @@ package book_app;
 public class Book {
 
     private String author;
-    protected String title;
-    protected int yearPublished;
-    protected CallNumber callnum;
+    private String title;
+    private int yearPublished;
+    private CallNumber callnum;
 
     public Book(String author, String title, int yearPublished, CallNumber callnum) {
         this.author = author;
@@ -19,7 +19,7 @@ public class Book {
         String genre;
         String location;
 
-        genre = switch (callnum.getCallNum().substring(0, 2)) {
+        genre = switch (getCallnum().getCallNum().substring(0, 2)) {
             case "CT" -> "Information Technology";
             case "EE" -> "Engineering";
             case "SC" -> "Arts & Social Sciences";
@@ -28,7 +28,7 @@ public class Book {
             default -> "Unknown";
         };
 
-        location = switch (callnum.getCallNum().substring(2, 5)) {
+        location = switch (getCallnum().getCallNum().substring(2, 5)) {
             case "001" -> "Level One";
             case "002" -> "Level Two";
             case "003" -> "Level Three";
@@ -38,8 +38,8 @@ public class Book {
         };
 
         System.out.printf("Author      >> %s", getAuthor());
-        System.out.printf("\nTitle       >> %s", title);
-        System.out.printf("\nYear        >> %d", yearPublished);
+        System.out.printf("\nTitle       >> %s", getTitle());
+        System.out.printf("\nYear        >> %d", getYearPublished());
         System.out.printf("\nGenre       >> %s", genre);
         System.out.printf("\nLocation    >> %s\n\n", location);
         
@@ -51,6 +51,30 @@ public class Book {
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public CallNumber getCallnum() {
+        return callnum;
+    }
+
+    public void setCallnum(CallNumber callnum) {
+        this.callnum = callnum;
     }
 
 }
